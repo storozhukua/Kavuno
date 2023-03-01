16d1a212fb18cda0476a3261b9fd1a382532e157
istorozh <Igor.Storozhuk@openbet.com>
2023-03-01 23:07:10 +0200

    add test - Render Timer with start btn

--- src/tests/components/timer.test.js
+++ src/tests/components/timer.test.js
@@ -1,10 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Timer from '../../components/timer/Timer';
+import { useTimer } from '../../contexts/TimerContext'
 
-test('render Timer', () => {
-  render(<Timer />);
-  
-  
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
+jest.mock('../../contexts/TimerContext', () => {
+  return {
+    ...jest.requireActual('../../contexts/TimerContext'),
+    useTimer: jest.fn(),
+  }});
+
+beforeEach(() => {
+  useTimer.mockReturnValue({
+    currentKey: 0,
+    options: [
+      {
+        duration: 60,
+        type: "learn"
+      },
+      {
+        duration: 60,
+        type: "rest"
+      }
+    ]
+  })
+
+  render(<Timer/>)
+});
+
+test('Render Timer with start btn', () => {
+  const startButton = screen.getByRole('button');
+
+  expect(startButton).toHaveTextContent(/start/i);
+});
+
+test('Click on start button', () => {
+  const startButton = screen.getByRole('button');
+
+  fireEvent.click(startButton);
+
+  expect(startButton).toHaveTextContent(/stop/i);
 });
